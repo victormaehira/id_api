7a11df319b34c85d008647ed043a0d665370545b
victor.maehira <vymaehira@gmail.com>
2022-11-13 21:48:05 -0300

    Spring boot dependencies added.

---
+++ src/main/java/com/victor/gke/idwallet/IdwalletApplication.java
@@ -0,0 +1,13 @@
+package com.victor.gke.idwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdwalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victor/gke/idwallet/IdwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.gke.idwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
